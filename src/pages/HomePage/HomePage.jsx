6ccb52ba8707725911e4c0e9edda8b948fad4d05
Alan Son <alanson76@gmail.com>
2020-08-18 21:08:10 -0700

    components broaken out to smaller components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,53 +1,12 @@
 import React from 'react';
 
 import './HomePage.scss';
+import Directory from '../../components/Directory/Directory';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <div className='directory-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Show Now</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Show Now</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Show Now</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Show Now</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Show Now</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Show Now</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
